ca8b64a9dae748b485297bc81c0b1c38227935ad
Dadjt5 <dadjt5@gmail.com>
2024-03-25 22:15:35 +0100

    a

--- persona/settings.py
+++ persona/settings.py
@@ -128,5 +128,5 @@ STATIC_ROOT = 'api/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ['http://localhost:5173', 'http://localhost:8080',]
+CORS_ORIGIN_WHITELIST = ['http://localhost:5173', 'http://localhost:8080', 'http://clientevue.onrender.com',]
 
