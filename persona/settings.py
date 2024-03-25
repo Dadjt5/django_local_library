1abbdfba89d4625964c1f15f70f1715aaf1df40a
Dadjt5 <dadjt5@gmail.com>
2024-03-25 12:46:11 +0100

    static

--- persona/settings.py
+++ persona/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-it*qkau_mddd@bv!fzj4)=ja_vu4_g_bb*n%@+cldh%936(r6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
